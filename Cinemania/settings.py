16b9aeadc027c7310e82dbba6f0f567116c857dd
sabatohura <sabato@hura.one>
2021-09-13 06:17:51 +0200

    Update settings.py
    Co-Authored-By: Ikuze Euphrosine <85740486+Euphrosine@users.noreply.github.com>

--- Cinemania/settings.py
+++ Cinemania/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uy6v$pjv7o=6&phhg(t98v8r(pctzcvd=49v*mb)$5@htrf(mr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['cinemania-spacex.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','cinemania-spacex.herokuapp.com']
 
 # Application definition
 
