750fa932a7f6d0c2edf21d88ff5a403519008b27
sabatohura <sabato@hura.one>
2021-09-13 05:59:48 +0200

    Update settings.py
    Co-Authored-By: Ikuze Euphrosine <85740486+Euphrosine@users.noreply.github.com>

--- Cinemania/settings.py
+++ Cinemania/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-uy6v$pjv7o=6&phhg(t98v8r(pctzcvd=49v*mb)$5@htrf(mr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cinemania-spacex.herokuapp.com']
 
 # Application definition
 
